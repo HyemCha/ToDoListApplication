7889507f3547eb094cf22e9e0e8cf8f99f9e6f8f
HyemCha <chm6194@gmail.com>
2022-09-03 22:35:49 +0900

    controller test

---
+++ demo/src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO { //HTTP 응답으로 사용
+    private String error;
+    private List<T> data;
+}
---
+++ demo/src/main/java/com/example/demo/dto/TodoDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.TodoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO { //Todo 아이템을 생성, 수정, 삭제
+    private String id;
+    private String title;
+    private boolean done;
+
+    public TodoDTO(final TodoEntity entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.done = entity.isDone();
+    }
+}
